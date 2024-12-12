288872fcbd09d096333d9376bd6fd2a06f299939
Erlangga Tresnamada Muliawan <tresnamada@gmail.com>
2024-12-13 00:24:38 +0700

    (+)Fixed Quiz, 404 Page, Standby, Responsive Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
     'video.html',
     'makanan.html',
     'komunitas.html',
+    'ulasan.html',
+    'quiz.html',
+    'standby.html',
+    'test.html',
+    '404.html',
     './src/**/*.html',  // atau direktori HTML lainnya
   ],
   theme: {
@@ -21,6 +26,22 @@ module.exports = {
         garamond: ["EB Garamond", "serif"],
       },
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
